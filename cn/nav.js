cb5b1ed9eac90516cf093e7dcefa8cdfbb8d6ecf
liushidong11 <42347905+liushidong11@users.noreply.github.com>
2023-05-25 08:44:32 +0800

    Add files via upload

---
+++ cn/nav.js
@@ -0,0 +1,7 @@
+document.writeln("		<nav>");
+document.writeln("    		<ul>");
+document.writeln("        		<li><a href=\"../cn/index.html\">主页</a></li>");
+document.writeln("        		<li><a href=\"teaching.html\">教学</a></li>");
+document.writeln("        		<li><a href=\"doc.html\">文档</a></li>");
+document.writeln("    		</ul>");
+document.writeln("		</nav>");
---
+++ en/nav.js
@@ -0,0 +1,7 @@
+document.writeln("		<nav>");
+document.writeln("    		<ul>");
+document.writeln("        		<li><a href=\"index.html\">Home</a></li>");
+document.writeln("        		<li><a href=\"teaching.html\">Teaching</a></li>");
+document.writeln("        		<li><a href=\"doc.html\">Documents</a></li>");
+document.writeln("    		</ul>");
+document.writeln("		</nav>");
