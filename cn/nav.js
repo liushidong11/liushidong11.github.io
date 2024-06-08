edc002832ec582cc9b3454275d6b1f4266ffff07
liushidong11 <liushidong11@foxmail.com>
2024-06-08 16:55:08 +0800

    添加一个杂记测试

--- cn/nav.js
+++ cn/nav.js
@@ -3,5 +3,6 @@ document.writeln("    		<ul>");
 document.writeln("        		<li><a href=\"../cn/index.html\">主页</a></li>");
 document.writeln("        		<li><a href=\"teaching.html\">教学</a></li>");
 document.writeln("        		<li><a href=\"doc.html\">文档</a></li>");
+document.writeln("        		<li><a href=\"zaji.html\">杂记</a></li>");
 document.writeln("    		</ul>");
 document.writeln("		</nav>");
