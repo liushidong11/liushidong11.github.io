a04810c9664bdfaab560f42ac0cea6e4c34de11e
liushidong11 <liushidong11@foxmail.com>
2024-06-08 17:06:15 +0800

    右上中文删掉后正文排版上移，故不能删掉

--- en/nav.js
+++ en/nav.js
@@ -3,5 +3,6 @@ document.writeln("    		<ul>");
 document.writeln("        		<li><a href=\"index.html\">Home</a></li>");
 document.writeln("        		<li><a href=\"teaching.html\">Teaching</a></li>");
 document.writeln("        		<li><a href=\"doc.html\">Documents</a></li>");
+document.writeln("        		<li><a href=\"zaji.html\">ZaJi</a></li>");
 document.writeln("    		</ul>");
 document.writeln("		</nav>");
